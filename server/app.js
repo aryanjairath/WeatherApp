b4d7907651e3be01356a524e0dbb81dfb4435235
aryanjairath <aryanjairath7@gmail.com>
2025-01-15 00:05:54 -0500

    sign in database config

--- server/app.js
+++ server/app.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const axios = require('axios');
+const mysql = require("mysql")
+const dotenv = require('dotenv')
 const app = express();
+const mongoose = require('mongoose')
 const cors = require('cors'); // If you need CORS for local dev
-app.use(cors()); // only if needed for development
+const User = require('./models/User');
+require('dotenv').config();
 
-const expressListEndpoints = require('express-list-endpoints');
-const { useParams } = require('react-router-dom');
+app.use(cors()); // only if needed for development
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 
 const API_KEY = 'LXFZWTASLFAVQ2QD5BZGP2UPC';
@@ -37,11 +49,52 @@ app.get('/api/news/:loc', async (req, res) => {
     res.status(500).json({error: 'Failed to fetch news' });
   }
 });
+
+app.post('/api/signup', async (req, res) => {
+  try{
+    const {username, password, email} = req.body;
+
+    if (!username || ! email || !password) {
+      return res.status(400).json({error: 'Missing a required field'});
+    }
+
+    const existingUser = await User.findOne({ $or: [ {username}, {email} ] });
+    if (existingUser){
+      return res.status(400).json({error: 'Username or email already exists'});
+    }
+
+    const newUser = new User({
+      username, password, email
+    });
+
+    await newUser.save();
+    res.status(201).json({message:"User created successfully", username: newUser.username});
+  } catch(error) {
+    console.error('Error registering user: ',error);
+    res.status(500).json({error: 'Server error'});
+  }
+});
+
+app.post('/api/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      res.status(400).json({error: 'Missing key field'});
+    }
+    const currUser = await User.findOne({ username });
+    if (!currUser){
+      res.status(400).json({error: "No such user"});
+    }
+    console.log(currUser.username);
+  } catch (error) {
+    console.error('Error loggin in', error);
+  }
+  res.json({message: 'Loggined in successfully'})
+});
 // Start the server on port 5000 (or your choice)
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-console.log(expressListEndpoints(app));
 
    
